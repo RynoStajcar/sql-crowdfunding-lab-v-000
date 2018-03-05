bca325aca34e2615989a1b0405c591a9aac983b4
Ryne Stajcar <rynostajcar@gmail.com>
2018-03-05 16:26:17 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -25,7 +25,7 @@ end
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
 "SELECT Projects.title, Pledges.amount
 FROM Projects
-INNER JOIN Users
+INNER JOIN Pledges
 ON Projects.id = Pledges.user_id
 "
 end
